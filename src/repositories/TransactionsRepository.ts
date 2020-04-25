374aa15462e45cfa7c6440d061fc5cacfae773f0
jefferson-luis-nascimento <je.luis.nascimento@hotmail.com>
2020-04-25 14:34:46 -0300

    Transaction operations

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionsRepositoryDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter(tran => tran.type === 'income')
+      .reduce((amount, current) => {
+        return amount + current.value;
+      }, 0);
+    const outcome = this.transactions
+      .filter(tran => tran.type === 'outcome')
+      .reduce((amount, current) => {
+        return amount + current.value;
+      }, 0);
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({
+    title,
+    value,
+    type,
+  }: TransactionsRepositoryDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
